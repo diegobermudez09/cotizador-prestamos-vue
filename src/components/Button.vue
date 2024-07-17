c1bd931f33be8d40d0aa5b77d8b14a9a0999e244
diegobermudez09 <diegobl9do@gmail.com>
2024-07-17 17:59:11 -0500

    colocando el resumen de pagos

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -7,7 +7,7 @@
         <button
         class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2
         hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
-        @click="fn"
+        @click="$emit('fn')"
         >    
         {{operador}}</button>
     </div>
