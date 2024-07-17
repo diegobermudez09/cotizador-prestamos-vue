a0ea618d1569a01e5c9460a1e863cf8cd0ce207d
diegobermudez09 <diegobl9do@gmail.com>
2024-07-17 16:44:38 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+import Header from './components/Header.vue';
+
+const cantidad = ref(10000);
+const MIN = 0;
+const MAX = 20000;
+const STEP = 100;
+
+
+
+function handleChange(e) {
+  cantidad.value = +e.target.value;
+}
+</script>
+
+<template>
+  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+
+    <div class="my-5">
+      <input 
+        type="range"
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600" 
+        :min="MIN" 
+        :max="MAX"
+        :step="STEP"
+        :value="cantidad"
+        @input="handleChange"
+      />
+      <p>$ {{cantidad}}</p>
+   
+
+    </div>
+  </div>
+</template>
